7e7858f2b57f3962613a9a6ca50f5897dcac2734
OHM79 <ruin_of_messnger@yahoo.co.jp>
2017-12-03 10:11:15 +0900

    演習問題 1－2 回答

--- 1-2/Calc1.java
+++ 1-2/Calc1.java
@@ -6,15 +6,19 @@
 
 public class Calc1 {
 
-    public static void main(String args[]) {
-        // 販売データ
-        int data[]
-            = {32, 27, 80, 120, 76, 18, 212, 320, 53, 44, 61, 420};
-        
-        int   total = data[0];          // 合計
-        float ave   = 0.0f;             // 平均値
-        
-        // 必要な処理を記述し、Calc1クラスを作成してください。
+	public static void main(String args[]) {
+		// 販売データ
+		int data[] = { 32, 27, 80, 120, 76, 18, 212, 320, 53, 44, 61, 420 };
 
-    }
+		int total = 0; // 合計
+		float ave = 0.0f; // 平均値
+
+		// 必要な処理を記述し、Calc1クラスを作成してください。
+		for (int i : data) {
+			total += i;
+		}
+		ave = total / (float) data.length;
+		System.out.println("平均値 >>" + ave + "個");
+		System.out.println("合計 >> " + total + "個");
+	}
 }
