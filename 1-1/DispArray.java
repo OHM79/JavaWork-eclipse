df56fd55997704800a8c0834baeebafd9bda724d
OHM79 <ruin_of_messnger@yahoo.co.jp>
2017-12-03 10:10:47 +0900

    演習問題 1-1回答

--- 1-1/DispArray.java
+++ 1-1/DispArray.java
@@ -6,13 +6,21 @@
 
 public class DispArray {
 
-    public static void main(String args[]) {
-        // 販売データ
-        int data[]
-            = {32, 27, 80, 120, 76, 18, 212, 320, 53, 44, 61, 420};
-        
-        // 必要な処理を記述し、DispArrayクラスを作成してください。
+	public static void main(String args[]) {
+		// 販売データ
+		int data[] = { 32, 27, 80, 120, 76, 18, 212, 320, 53, 44, 61, 420 };
+		int soldItemsGraph = 0;
+		// 必要な処理を記述し、DispArrayクラスを作成してください。
+		for (int i = 0; i < data.length; i++) {
+			//			System.out.println(data[i]);
+			soldItemsGraph = (int) Math.round(data[i] / 10.0);
+			//			System.out.println(soldItemsGraph);
+			System.out.print(i + 1 + "月\t|");
+			for (int j = 0; j < soldItemsGraph; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 
-
-    }
+	}
 }
