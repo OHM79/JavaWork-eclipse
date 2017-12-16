22e410f128307ff725daaa3e93491324cb3e07a0
OHM79 <ruin_of_messnger@yahoo.co.jp>
2017-12-16 13:00:25 +0900

    タブをスペースに変換

--- C2Lab4/src/CatorDog.java
+++ C2Lab4/src/CatorDog.java
@@ -7,26 +7,26 @@
 // ペット所有者カウントクラス
 class CatorDog {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		// ペット
-		String pets[] = { "猫", "犬", "猫", "猫", new String("猫"),
-				"猫", "犬", "犬", "猫", new String("犬") };
-		int dogCount = 0;
-		int catCount = 0;
+        // ペット
+        String pets[] = { "猫", "犬", "猫", "猫", new String("猫"),
+                "猫", "犬", "犬", "猫", new String("犬") };
+        int dogCount = 0;
+        int catCount = 0;
 
-		// 必要な変数および処理を記述してください。
-		for (String pet : pets) {
-			if (pet.equals("猫")) {
-				//				System.out.println("猫");
-				catCount++;
-			} else if (pet.equals("犬")) {
-				dogCount++;
-			} else {
+        // 必要な変数および処理を記述してください。
+        for (String pet : pets) {
+            if (pet.equals("猫")) {
+                //				System.out.println("猫");
+                catCount++;
+            } else if (pet.equals("犬")) {
+                dogCount++;
+            } else {
 
-			}
-		}
-		System.out.println("猫を飼っている人は" + catCount + "人です。");
-		System.out.println("犬を飼っている人は" + dogCount + "人です。");
-	}
+            }
+        }
+        System.out.println("猫を飼っている人は" + catCount + "人です。");
+        System.out.println("犬を飼っている人は" + dogCount + "人です。");
+    }
 }
