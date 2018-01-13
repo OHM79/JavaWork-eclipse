3d0764d2371a0386c3ea9e74e1144b69f6e600fb
OHM79 <ruin_of_messnger@yahoo.co.jp>
2018-01-13 17:37:21 +0900

    演習4－1 実装

--- C4Lab1/src/com/fjqs/java/labs/chap4/Employee.java
+++ C4Lab1/src/com/fjqs/java/labs/chap4/Employee.java
@@ -7,3 +7,16 @@
 package com.fjqs.java.labs.chap4;
 
 // 必要なメンバ変数およびメソッドを持つ Employeeクラスを定義してください。
+public class Employee extends SchoolStaff {
+	private String description;
+
+	public Employee(String name, int age, String description) {
+		super(name, age);
+		this.description = description;
+	}
+
+	public void print() {
+		super.print();
+		System.out.println(this.description + "です。");
+	}
+}
--- C4Lab1/src/com/fjqs/java/labs/chap4/Student.java
+++ C4Lab1/src/com/fjqs/java/labs/chap4/Student.java
@@ -7,3 +7,16 @@
 package com.fjqs.java.labs.chap4;
 
 // 必要なメンバ変数およびメソッドを持つ Studentクラスを定義してください。
+public class Student extends SchoolStaff {
+	private String grade;
+
+	public Student(String name, int age, String grade) {
+		super(name, age);
+		this.grade = grade;
+	}
+
+	public void print() {
+		super.print();
+		System.out.println(this.grade + "です。");
+	}
+}
