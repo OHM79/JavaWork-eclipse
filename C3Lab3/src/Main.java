c1c1abec4c67cc9bc2fc7ab586593d2830e6e5a7
OHM79 <ruin_of_messnger@yahoo.co.jp>
2018-01-13 12:37:00 +0900

    演習3-3 実装

--- C3Lab3/src/Main.java
+++ C3Lab3/src/Main.java
@@ -6,22 +6,22 @@
 
 // メインクラス
 class Main {
-	
-	public static void main(String args[]) {
-		
-		// インスタンスの生成
-		Vegetable spinach = new Vegetable("ほうれん草", "束", 200);
-		
-		// 注文処理
-		spinach.order(5);
-		
-		// 情報の表示
-		System.out.println("------------------------------");
-		spinach.display();
-		System.out.println("------------------------------");
-		spinach.displayTotal();
-		System.out.println("------------------------------");
-		
-	}
+
+    public static void main(String args[]) {
+
+        // インスタンスの生成
+        Vegetable spinach = new Vegetable("ほうれん草", "束", 200);
+
+        // 注文処理
+        spinach.order(5);
+
+        // 情報の表示
+        System.out.println("------------------------------");
+        spinach.display();
+        System.out.println("------------------------------");
+        spinach.displayTotal();
+        System.out.println("------------------------------");
+
+    }
 }
 
--- C3Lab3/src/Vegetable.java
+++ C3Lab3/src/Vegetable.java
@@ -6,7 +6,33 @@
 
 // 野菜クラス
 class Vegetable {
-	
-	// 必要なメンバ変数およびメソッドを記述してください。
+
+    String name;
+    String unit;
+    int price;
+    int quantity;
+
+    public Vegetable(String name,String unit,int price) {
+        this.name = name;
+        this.unit = unit;
+        this.price = price;
+    }
+
+    public void order(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void display() {
+        System.out.println("品名   : " + this.name);
+        System.out.println("単位   : " + this.unit);
+        System.out.println("単価   : " + this.price);
+        System.out.println("注文数 : " + this.quantity);
+    }
+
+    public void displayTotal() {
+        int total = this.price * this.quantity;
+        System.out.println("売上金額 : " + total);
+    }
+
 }
 
