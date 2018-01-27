7b5b3f0393682c987e56aaffb107be7fb13f0690
OHM79 <ruin_of_messnger@yahoo.co.jp>
2018-01-27 17:56:12 +0900

    C4Lab4の作成

--- C4Lab4/src/com/fjqs/java/labs/chap4/LaborStudent.java
+++ C4Lab4/src/com/fjqs/java/labs/chap4/LaborStudent.java
@@ -9,10 +9,28 @@ package com.fjqs.java.labs.chap4;
 // 勤労学生クラス
 public class LaborStudent implements LaborStudentInterface {
 
-    private EmployeeInterface emp;       // 従業員
-    private StudentInterface stu;        // 生徒
-    
-    // 必要な処理を記述し、LaborStudentクラスを作成してください。
+	private EmployeeInterface emp;       // 従業員
+	private StudentInterface stu;        // 生徒
 
+	public LaborStudent(String name, String grade, String description, int annualIncome) {
+		emp = new Employee(name, description, annualIncome);
+		stu = new Student(name, grade);
+	}
+
+	public void printName() {
+		stu.printName();
+	}
+
+	public void printGrade() {
+		stu.printGrade();
+	}
+
+	public void printDescription() {
+		emp.printDescription();
+	}
+
+	public void printAnnualIncome() {
+		emp.printAnnualIncome();
+	}
 }
 
