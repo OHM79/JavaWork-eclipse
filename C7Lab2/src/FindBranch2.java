fa7b0e5aaa46431638845a02e514f10340cd93e0
OHM79 <ruin_of_messnger@yahoo.co.jp>
2018-02-24 18:07:04 +0900

    演習7-2作成中

--- C7Lab2/src/FindBranch2.java
+++ C7Lab2/src/FindBranch2.java
@@ -4,38 +4,72 @@
  *   著作権表示      ：Copyright 2015 Fujitsu Kyushu Systems Limited
  */
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 // 支店・配送車検索クラス
 public class FindBranch2 {
 
-    public static void main(String args[]) {
-    	
-        // ドライバクラス
-        String driver = "com.mysql.jdbc.Driver";
-        // データベースのURL
-        String url    = "jdbc:mysql://localhost:3306/fjqs_DB";
+	public static void main(String args[]) {
+
+		// ドライバクラス
+		String driver = "com.mysql.jdbc.Driver";
+		// データベースのURL
+		String url = "jdbc:mysql://localhost:3306/fjqs_DB";
 		// データベースのユーザ
 		String  user  = "root";
 		// パスワード
 		String  pass  = "password";
-        
-        Connection         con   = null;      // コネクション
-        PreparedStatement  stmt1 = null;      // ステートメント（支店用）
-        PreparedStatement  stmt2 = null;      // ステートメント（配送車用）
-        ResultSet          res1  = null;      // 結果セット（支店用）
-        ResultSet          res2  = null;      // 結果セット（配送車用）
-        
-        String code = "";            		  //支店コード
-        if(args.length == 1) {
-            code = args[0];
-        }
-        else {
-            System.out.println("支店コードを指定してください");
-            System.exit(-1);
-        }
 
-        // 必要なデータベースアクセス処理を記述してください。
-    	
-    }
+		Connection con = null;      // コネクション
+		PreparedStatement stmt = null;      // ステートメント（支店用）
+		PreparedStatement stmt1 = null;      // ステートメント（支店用）
+		PreparedStatement stmt2 = null;      // ステートメント（配送車用）
+		ResultSet res = null;      // 結果セット（支店用）
+		ResultSet res1 = null;      // 結果セット（支店用）
+		ResultSet res2 = null;      // 結果セット（配送車用）
+
+		String code = "";            		  // 支店コード
+		if (args.length == 1) {
+			code = args[0];
+		}
+		else {
+			System.out.println("支店コードを指定してください");
+			System.exit(-1);
+		}
+
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, pass);
+			String sql = "select * from BRANCH as bra " +
+					"inner join DELIVERY_VAN as van " +
+					"on bra.CODE =van.CODE WHERE bra.CODE = ?;";
+			stmt = con.prepareStatement(sql);
+			stmt.setString(1, code);
+			res = stmt.executeQuery();
+
+			if (res.next()) {
+				String cod = res.getString("CODE");
+				String address = res.getString("ADDRESS");
+				String tel = res.getString("TEL");
+				String TOTAL = res.getString("TOTAL");
+
+				System.out.println("--------支店情報----------");
+				System.out.println("支店コード		:" + cod);
+				System.out.println("住所			:" + address);
+				System.out.println("電話番号		:" + tel);
+				System.out.println("未配達総数		:" + TOTAL);
+			} else {
+				System.out.println("支店コード '" + code + "' に対応する情報は見つかりませんでした。");
+			}
+
+		} catch (SQLException e) {
+			System.out.println(e);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
